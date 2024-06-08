29e7b0acd1b809d6b50b1df7c00de479061407ad
Duke-Leslie <dukelesliemua@gamil.com>
2024-06-08 12:06:48 +0100

    added: john.js

---
+++ john.js
@@ -0,0 +1,2 @@
+const mes = "New File"
+console.log(`This is duke's ${mes}`)
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const name = "Duke"
 const color = "white"
-// ES6( ECMA Script 6 ) Wow
+// ES6( ECMA Script 6 ) 
 
 console.log(`My name is ${name} and I love ${color} color.`)
